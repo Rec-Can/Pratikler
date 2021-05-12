df8f91110d3de311bb9dd6b5ea9676f12e957882
Rec-Can <79525152+Rec-Can@users.noreply.github.com>
2021-05-12 18:11:47 +0300

    değişkenler

---
+++ Değişkenler/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Değişkenler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string dateTime = DateTime.Now.ToString("dd.MM.yyyy");
+            Console.WriteLine(dateTime);
+        }
+    }
+}
