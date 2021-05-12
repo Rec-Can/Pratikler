3cef7b322d0d0aa8708bd92e0a0fa41f68f988bf
Rec-Can <79525152+Rec-Can@users.noreply.github.com>
2021-05-12 18:41:26 +0300

    operatörler

---
+++ Operatörler/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Operatörler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            int x = 3;
+            int y = 4;
+            int z = x + y;
+
+            bool isSuccess = true;
+            bool isCompleted = false;
+
+            Console.WriteLine(y);
+            y += 2;
+            Console.WriteLine(x+y);
+            z *= 2;
+            Console.WriteLine(z);
+
+            if (isSuccess)
+            {
+                Console.WriteLine(z/x);
+            }
+            if (isSuccess && !isCompleted)
+            {
+                Console.WriteLine(z%y);
+            }
+        }
+    }
+}
