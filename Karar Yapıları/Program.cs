b91a9f7b8af4ea438ecfd0cb39e37dc9938a7677
Rec-Can <79525152+Rec-Can@users.noreply.github.com>
2021-05-13 11:08:28 +0300

    Karar Yapıları

---
+++ Karar Yapıları/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Karar_Yapıları
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+            // if-else-elseif
+            if(time>= 6 && time<12){
+                Console.WriteLine("Günaydın");
+            }
+
+            else if( time <= 12)
+            {
+                Console.WriteLine("İyi Günler");
+            }
+            else{
+                Console.WriteLine("İyi Geceler");
+            }
+            // Ternary 
+
+            // string sonuc = time <= 18 ? "İyi Günler" : "İyi Geceler";
+
+            string sonuc = time>= 6 && time<12 ? "Günaydın" : time <= 12 ? "İyi Günler" : "İyi Geceler";
+            Console.WriteLine(sonuc);
+        }
+    }
+}
