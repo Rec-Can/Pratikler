0d9339fcb84f538f8fb5b765f129a42bd57b95ae
Rec-Can <79525152+Rec-Can@users.noreply.github.com>
2021-05-13 13:34:55 +0300

    For-Break-Continue

---
+++ For-Break-Continue/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace For_Break_Continue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Sayı Giriniz");
+            int sayac =  int.Parse(Console.ReadLine());
+            for ( int i=0; i<= sayac; i++){
+                if(i%2 ==1){
+                    Console.WriteLine(i);
+                    }
+            }
+
+        }
+    }
+}
