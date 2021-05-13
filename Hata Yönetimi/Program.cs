5b9e8834c67640b36aeda4f536596bf91dcc6341
Rec-Can <79525152+Rec-Can@users.noreply.github.com>
2021-05-13 11:00:47 +0300

    Hata Yönetimi

---
+++ Hata Yönetimi/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Hata_Yönetimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try{Console.WriteLine("Write a Number");
+            int deger = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Girmiş olduğunuz sayı " +deger);
+            }
+            catch(Exception ex){
+                Console.WriteLine("Hata" +ex.Message.ToString());
+            }
+            finally{
+                Console.WriteLine("İşlem Tamamlandı!");
+            }
+        }
+    }
+}
