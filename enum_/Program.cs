1b4cfe5441943b5affc280005c1b2628e367391a
Rec-Can <79525152+Rec-Can@users.noreply.github.com>
2021-05-15 15:43:17 +0300

    enum_

---
+++ enum_/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace enum_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Günler.Pazar);
+        }
+        enum Günler
+    {
+        Pazartesi,
+        Salı,
+        Çarşamba,
+        Perşembe,
+        Cuma,
+        Cumartesi,
+        Pazar
+    }
+    }
+}
