6621b2f5a9455bdea5e5dbf1ed53f05c142a5a6f
Rec-Can <79525152+Rec-Can@users.noreply.github.com>
2021-05-16 19:59:27 +0300

    second commit
    kart ekleme ve board gösterimi tamamlandı

--- proje2/Program.cs
+++ proje2/Program.cs
@@ -1,18 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace proje2
+namespace Proje_2
 {
     class Program
-    {
-        
+    {  
         public static List<Kartlar> toDo = new List<Kartlar>();
         public static List<Kartlar> progress = new List<Kartlar>();
         public static List<Kartlar> done = new List<Kartlar>();
+        public static Dictionary<int, TakımÜyeleri> takımÜyeleri = new Dictionary<int, TakımÜyeleri>(){
+            { 101, new TakımÜyeleri {id=101, isim_soyisim="Recep"}},
+            { 102, new TakımÜyeleri {id=102, isim_soyisim="Çağatay "}},
+            { 103, new TakımÜyeleri {id=103, isim_soyisim="Büşra"}},
+            { 104, new TakımÜyeleri {id=104, isim_soyisim="Sılay"}},
+            { 105, new TakımÜyeleri {id=105, isim_soyisim="Ahmet"}} };
         static void Main(string[] args)
         {
-            TakımUyesiOlustur();
-            KartOlustur();
             int key = 0;
             while (key < 1 || key > 4)
             {
@@ -61,6 +64,7 @@ namespace proje2
 
         public static void BoardaKartEkleme()
         {
+            Console.Clear();
             Kartlar yeniKart = new Kartlar();
 
             Console.WriteLine("Başlık giriniz                                      : ");
@@ -93,23 +97,34 @@ namespace proje2
             }
 
             Console.WriteLine("Kişi seçiniz                                        : ");
-            yeniKart.atanankisi = Console.ReadLine();
-
-            Console.WriteLine("Hangi Line'a eklemek istiyorsunuz? -> ToDo (1), Progress (2), Done (3) :");
-            int line = Convert.ToInt32(Console.ReadLine());
-            if (line == 1)
+            int keytemp = Convert.ToInt32(Console.ReadLine());
+            if (takımÜyeleri.ContainsKey(keytemp))
             {
-                toDo.Add(yeniKart);
+                yeniKart.atanankisi = takımÜyeleri[keytemp];
+                Console.WriteLine("Hangi Line'a eklemek istiyorsunuz? -> ToDo (1), Progress (2), Done (3) :");
+                int line = Convert.ToInt32(Console.ReadLine());
+                if (line == 1)
+                {
+                    toDo.Add(yeniKart);
+                }
+                if (line == 2)
+                {
+                    progress.Add(yeniKart);
+                }
+                if (line == 3)
+                {
+                    done.Add(yeniKart);
+                }
+                Console.WriteLine("Başarıyla eklendi.");
+                Console.ReadKey();
+                Main(null);
             }
-            if (line == 2)
+            else
             {
-                progress.Add(yeniKart);
-            }
-            if (line == 3)
-            {
-                done.Add(yeniKart);
-            }
-            Main(null);
+                Console.WriteLine("Hatalı giriş yaptınız.");
+                Console.ReadKey();
+                BoardaKartEkleme();
+            }           
         }
 
         public static void BoardListeleme()
@@ -121,50 +136,43 @@ namespace proje2
             {
                 Console.WriteLine("Başlık : " + done[i].baslik);
                 Console.WriteLine("İçerik : " + done[i].icerik);
-                Console.WriteLine("Atanan Kişi : " + done[i].atanankisi);
+                Console.WriteLine("Atanan Kişi : " + done[i].atanankisi.isim_soyisim);
                 Console.WriteLine("Büyüklük: " + done[i].kartBuyuklugu);
                 Console.WriteLine("-----------------");
             }
             
             Console.WriteLine();
             Console.WriteLine("Progress List");
+            Console.WriteLine("******************");
             for (int i = 0; i < progress.Count; i++)
             {
                 Console.WriteLine("Başlık : " + progress[i].baslik);
                 Console.WriteLine("İçerik : " + progress[i].icerik);
-                Console.WriteLine("Atanan Kişi : " + progress[i].atanankisi);
+                Console.WriteLine("Atanan Kişi : " + progress[i].atanankisi.isim_soyisim);
                 Console.WriteLine("Büyüklük: " + progress[i].kartBuyuklugu);
                 Console.WriteLine("-----------------");
             }
             
             Console.WriteLine();
             Console.WriteLine("ToDo List");
+            Console.WriteLine("******************");
             for (int i = 0; i < toDo.Count; i++)
             {
                 Console.WriteLine("Başlık : " + toDo[i].baslik);
                 Console.WriteLine("İçerik : " + toDo[i].icerik);
-                Console.WriteLine("Atanan Kişi : " + toDo[i].atanankisi);
+                Console.WriteLine("Atanan Kişi : " + toDo[i].atanankisi.isim_soyisim);
                 Console.WriteLine("Büyüklük: " + toDo[i].kartBuyuklugu);
                 Console.WriteLine("-----------------");
             }
+            Console.WriteLine();
             Console.ReadKey();
             Main(null);
-        }
-        public static Dictionary<int, string> takımUyeleri = new Dictionary<int, string>();
-        public static void TakımUyesiOlustur()
-        {
-            takımUyeleri.Add(0, "Recep Can");
-            takımUyeleri.Add(1, "Çağatay");
-            takımUyeleri.Add(2, "Büşra");
-            takımUyeleri.Add(3, "Ali");
-            takımUyeleri.Add(4, "Veli");
         }
         public static void KartOlustur()
         {
             Kartlar kart_1 = new Kartlar();
             kart_1.baslik = "Proje taslağını çıkar.";
             kart_1.icerik = "Taslak";
-            kart_1.atanankisi = takımUyeleri[0];
             kart_1.kartBuyuklugu = KartBuyuklugu.M;
             kart_1.progress = 2;
             done.Add(kart_1);
@@ -172,7 +180,6 @@ namespace proje2
             Kartlar kart_2 = new Kartlar();
             kart_2.baslik = "Projeyi tamamla.";
             kart_2.icerik = "Proje";
-            kart_2.atanankisi = takımUyeleri[1];
             kart_2.kartBuyuklugu = KartBuyuklugu.XL;
             kart_2.progress = 1;
             progress.Add(kart_2);
@@ -180,7 +187,6 @@ namespace proje2
             Kartlar kart_3 = new Kartlar();
             kart_3.baslik = "Projeyi gite yükle";
             kart_3.icerik = "Proje";
-            kart_3.atanankisi = takımUyeleri[2];
             kart_3.kartBuyuklugu = KartBuyuklugu.XS;
             kart_3.progress = 2;
             toDo.Add(kart_3);
@@ -191,10 +197,18 @@ namespace proje2
     {
         XS, S, M, L, XL
     }
+    public class TakımÜyeleri
+    {
+        public int id { get; set; }
+        public string isim_soyisim;
+        
+    }
     
     public class Kartlar
     {
-        public string baslik, icerik, atanankisi;
+
+        public TakımÜyeleri atanankisi;
+        public string baslik, icerik;
         public int progress;
         public KartBuyuklugu kartBuyuklugu;
     }
